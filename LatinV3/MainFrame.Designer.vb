460993f2737984bb2f5cb599a50eacb7f0017dc1
Jason Liu <jason.liu21@education.nsw.gov.au>
2020-05-03 15:20:20 +1000

    Add project files.

---
+++ LatinV3/MainFrame.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainFrame
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainFrame))
+        Me.animateShow = New System.Windows.Forms.Timer(Me.components)
+        Me.animateHide = New System.Windows.Forms.Timer(Me.components)
+        Me.holdTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.interLoad = New System.Windows.Forms.Timer(Me.components)
+        Me.b_forward = New System.Windows.Forms.Button()
+        Me.auxBrowser = New System.Windows.Forms.WebBrowser()
+        Me.b_back = New System.Windows.Forms.Button()
+        Me.display = New System.Windows.Forms.TextBox()
+        Me.buttonCheck = New System.Windows.Forms.Timer(Me.components)
+        Me.nicon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.cms = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Version101ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ghost = New System.Windows.Forms.Label()
+        Me.moveTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.cms.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'animateShow
+        '
+        Me.animateShow.Interval = 20
+        '
+        'animateHide
+        '
+        Me.animateHide.Interval = 20
+        '
+        'holdTimer
+        '
+        Me.holdTimer.Interval = 400
+        '
+        'interLoad
+        '
+        Me.interLoad.Enabled = True
+        Me.interLoad.Interval = 5
+        '
+        'b_forward
+        '
+        Me.b_forward.Enabled = False
+        Me.b_forward.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.b_forward.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.b_forward.Location = New System.Drawing.Point(26, 1)
+        Me.b_forward.Name = "b_forward"
+        Me.b_forward.Size = New System.Drawing.Size(26, 23)
+        Me.b_forward.TabIndex = 2
+        Me.b_forward.Text = ">"
+        Me.b_forward.UseVisualStyleBackColor = True
+        '
+        'auxBrowser
+        '
+        Me.auxBrowser.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.auxBrowser.Location = New System.Drawing.Point(0, 25)
+        Me.auxBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.auxBrowser.Name = "auxBrowser"
+        Me.auxBrowser.Size = New System.Drawing.Size(400, 175)
+        Me.auxBrowser.TabIndex = 0
+        '
+        'b_back
+        '
+        Me.b_back.Enabled = False
+        Me.b_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.b_back.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.b_back.Location = New System.Drawing.Point(1, 1)
+        Me.b_back.Name = "b_back"
+        Me.b_back.Size = New System.Drawing.Size(26, 23)
+        Me.b_back.TabIndex = 2
+        Me.b_back.Text = "<"
+        Me.b_back.UseVisualStyleBackColor = True
+        '
+        'display
+        '
+        Me.display.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.display.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.display.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.display.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.display.Location = New System.Drawing.Point(56, 2)
+        Me.display.Name = "display"
+        Me.display.Size = New System.Drawing.Size(340, 22)
+        Me.display.TabIndex = 3
+        '
+        'buttonCheck
+        '
+        Me.buttonCheck.Enabled = True
+        '
+        'nicon
+        '
+        Me.nicon.ContextMenuStrip = Me.cms
+        Me.nicon.Icon = CType(resources.GetObject("nicon.Icon"), System.Drawing.Icon)
+        Me.nicon.Text = "Wiktionquery"
+        Me.nicon.Visible = True
+        '
+        'cms
+        '
+        Me.cms.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem, Me.Version101ToolStripMenuItem})
+        Me.cms.Name = "cms"
+        Me.cms.Size = New System.Drawing.Size(181, 70)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Version101ToolStripMenuItem
+        '
+        Me.Version101ToolStripMenuItem.Enabled = False
+        Me.Version101ToolStripMenuItem.Name = "Version101ToolStripMenuItem"
+        Me.Version101ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Version101ToolStripMenuItem.Text = "Version 1.0.3"
+        '
+        'ghost
+        '
+        Me.ghost.AutoSize = True
+        Me.ghost.Location = New System.Drawing.Point(311, 76)
+        Me.ghost.Name = "ghost"
+        Me.ghost.Size = New System.Drawing.Size(0, 13)
+        Me.ghost.TabIndex = 4
+        '
+        'moveTimer
+        '
+        Me.moveTimer.Interval = 20
+        '
+        'MainFrame
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(400, 200)
+        Me.Controls.Add(Me.display)
+        Me.Controls.Add(Me.b_forward)
+        Me.Controls.Add(Me.b_back)
+        Me.Controls.Add(Me.auxBrowser)
+        Me.Controls.Add(Me.ghost)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "MainFrame"
+        Me.Opacity = 0.8R
+        Me.Text = "Wiktionquery"
+        Me.cms.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents animateShow As Timer
+    Friend WithEvents animateHide As Timer
+    Friend WithEvents holdTimer As Timer
+    Friend WithEvents interLoad As Timer
+    Friend WithEvents b_forward As Button
+    Friend WithEvents auxBrowser As WebBrowser
+    Friend WithEvents b_back As Button
+    Friend WithEvents display As TextBox
+    Friend WithEvents buttonCheck As Timer
+    Friend WithEvents nicon As NotifyIcon
+    Friend WithEvents cms As ContextMenuStrip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Version101ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ghost As Label
+    Friend WithEvents moveTimer As Timer
+End Class
---
+++ LatinV3/MainFrame.vb
@@ -0,0 +1,474 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class MainFrame
+
+    'Full disclosure: not my code, comes from someone online
+    'https://stackoverflow.com/questions/8543765/bring-vb-net-window-on-top-of-all-windows
+    Private Const SW_SHOWNOACTIVATE As Integer = 4
+    Private Const HWND_TOPMOST As Integer = -1
+    Private Const SWP_NOACTIVATE As UInteger = &H10
+    Private Const SW_SHOW As Integer = 5
+    Private Const SWP_SHOWWINDOW As UInteger = &H40
+
+    <DllImport("user32.dll", EntryPoint:="SetWindowPos")>
+    Private Shared Function SetWindowPos(ByVal hWnd As Integer, ByVal hWndInsertAfter As Integer, ByVal X As Integer, ByVal Y As Integer, ByVal cx As Integer, ByVal cy As Integer, ByVal uFlags As UInteger) As Boolean
+    End Function
+    <DllImport("user32.dll")>
+    Private Shared Function ShowWindow(ByVal hWnd As IntPtr, ByVal nCmdShow As Integer) As Boolean
+    End Function
+
+    Private Shared Sub ShowTopMost(ByVal frm As Form)
+        ShowWindow(frm.Handle, SW_SHOW)
+        SetWindowPos(frm.Handle.ToInt32(), HWND_TOPMOST, frm.Left, frm.Top, frm.Width, frm.Height, SWP_SHOWWINDOW)
+    End Sub
+
+    'Variables and stuff
+    Dim userOpacity As Double = 0.9
+    Dim map As New Dictionary(Of String, String)
+    Dim WithEvents kbh As New KeyboardHook
+    Dim qterm As String
+    Dim x As Integer
+    Dim y As Integer
+    Dim firstopen As Boolean = False
+    Dim prev As New List(Of String)
+    Dim prev_ind As Integer
+    Dim shift As Boolean = False
+    Dim ctrl As Boolean = False
+    Dim rctrl As Boolean = False
+
+    'Background keypress handlers
+    Private Sub kbh_KeyDown(Key As Keys) Handles kbh.KeyDown
+
+        If holdTimer.Enabled Then
+            Return
+        End If
+
+        If Key = Keys.LShiftKey Then
+            shift = True
+            If Visible And ctrl Then
+                animateHide.Enabled = True
+            End If
+        End If
+        If Key = Keys.LControlKey Then
+            ctrl = True
+            If Visible And shift Then
+                animateHide.Enabled = True
+            End If
+        End If
+        If Key = Keys.RControlKey Then
+            rctrl = True
+        End If
+        If shift And ctrl And Not Visible Then
+            holdTimer.Enabled = True
+        End If
+        If Key = Keys.M And (ctrl Or rctrl) And Visible Then
+            moveTimer.Enabled = Not moveTimer.Enabled
+        End If
+
+    End Sub
+
+    Private Sub kbh_KeyUp(Key As Keys) Handles kbh.KeyUp
+
+        If Key = Keys.LShiftKey Then
+            holdTimer.Enabled = False
+            shift = False
+        End If
+
+        If Key = Keys.LControlKey Then
+            holdTimer.Enabled = False
+            ctrl = False
+        End If
+
+    End Sub
+
+    'Misc animation stuff
+    Private Sub animateShow_Tick(sender As Object, e As EventArgs) Handles animateShow.Tick
+        If Visible = False Then
+            animateHide.Enabled = False
+            If Not Clipboard.ContainsText Then
+                Return
+            End If
+            Opacity = 0
+            firstopen = True
+            ShowTopMost(Me)
+            auxBrowser.Hide()
+            prev.Clear()
+            prev_ind = -1
+            auxBrowser.Navigate("https://en.wiktionary.org/wiki/" + Clipboard.GetText().Replace("ā", "a").Replace("ē", "e").Replace("ī", "i").Replace("ō", "o").Replace("ū", "u"))
+        End If
+        If Opacity < userOpacity Then
+            Opacity = Math.Min(Opacity + 0.1, userOpacity)
+        Else
+            Opacity = Math.Min(Opacity, userOpacity)
+            animateShow.Enabled = False
+        End If
+    End Sub
+
+    Private Sub animateHide_Tick(sender As Object, e As EventArgs) Handles animateHide.Tick
+        If Opacity <= userOpacity Then
+            animateShow.Enabled = False
+        End If
+        If Opacity > 0 Then
+            Opacity = Math.Max(Opacity - 0.1, 0)
+        Else
+            Opacity = Math.Max(Opacity, 0)
+            animateHide.Enabled = False
+            Hide()
+        End If
+    End Sub
+
+    Private Sub holdTimer_Tick(sender As Object, e As EventArgs) Handles holdTimer.Tick
+        animateShow.Enabled = True
+    End Sub
+
+    'Actual processing starts here
+    Dim parts As New List(Of String)({"Adjective", "Adverb", "Ambiposition", "Article", "Circumposition", "Classifier", "Conjunction", "Contraction", "Counter", "Determiner", "Ideophone", "Interjection", "Noun", "Numeral", "Participle", "Particle", "Postposition", "Preposition", "Pronoun", "Proper noun", "Verb"})
+
+    Private Sub auxBrowser_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles auxBrowser.DocumentCompleted
+
+        Dim lStart As HtmlElement
+
+        'Look for the start of the Latin section
+        Try
+            lStart = auxBrowser.Document.GetElementById("Latin").Parent.NextSibling
+        Catch
+            If Not qterm.ToLower = qterm Then
+                prev.RemoveAt(prev_ind)
+                prev_ind -= 1
+                auxBrowser.Navigate("https://en.wiktionary.org/wiki/" + qterm.Replace("ā", "a").Replace("ē", "e").Replace("ī", "i").Replace("ō", "o").Replace("ū", "u").ToLower)
+                Return
+            End If
+            display.Text = "Not a Latin word. | " & qterm
+            displayMem = "Not a Latin word. | " & qterm
+            ghost.Select()
+            Return
+        End Try
+
+        Dim str As String = ""
+        Dim senses As Integer = 0
+
+        'More fancy animations
+        Width = 400
+        Height = 200
+        rsz()
+
+        'Look over the HTML until you either find the start of the next section (or the end of the page)
+        Try
+
+            Do Until (lStart.TagName = "H2")
+
+                Try
+
+                    'If you see the start of a definition (which is like 'Verb' or 'Participle' on the webpage), add it to the list of things to display (which is just a string because HTML)
+                    If lStart.FirstChild.TagName = "SPAN" And parts.Contains(lStart.FirstChild.InnerText) Then
+
+                        str = str & lStart.OuterHtml & vbNewLine & lStart.NextSibling.OuterHtml & vbNewLine & lStart.NextSibling.NextSibling.OuterHtml & vbNewLine
+                        senses += 1
+
+                    End If
+
+                    'Add any tables seen
+                    Try
+                        If lStart.TagName = "TABLE" Then
+
+                            'Make the window bigger to show the table
+                            Width = 800
+                            Height = 400
+                            rsz()
+
+                            str = str & lStart.OuterHtml & vbNewLine
+
+                            'Replace all the colors in the table with darker colors for readability
+                            For Each c In map
+                                str = str.Replace(c.Key, c.Value)
+                            Next
+
+                        End If
+                    Catch
+                    End Try
+
+                Catch
+                End Try
+
+                'Go to next element
+                lStart = lStart.NextSibling
+
+                'Check for end of page
+                If lStart = Nothing Then
+                    Exit Do
+                End If
+
+            Loop
+
+        Catch
+            display.Text = "An error occurred. | " & qterm
+            displayMem = "An error occurred. | " & qterm
+            Return
+        End Try
+
+        'Fancy regex stuff, removes all the [edit]s on the page
+        str = System.Text.RegularExpressions.Regex.Replace(str, "<SPAN class=mw-editsection>.+?</SPAN></SPAN>", "")
+
+        '[hacky!] Make the webpage (basically chuck a bunch of CSS in the head and then dump the string we've been building up in the body, HTML does the rest for you)
+        auxBrowser.Document.Write("
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body { background-color: #282828; font-family: sans-serif  }
+            span, a, i, th, td, p, h3, h2, ol { color: #ffffff; font-size: 9pt; line-height: 1 }
+            th { background-color: #38C4B6 }
+            a { text-decoration: none }
+        </style>
+    </head>
+    <body>
+        " & str & "
+    </body>
+</html>")
+
+        'Darken the border of the table (readability)
+        Try
+            auxBrowser.Document.GetElementsByTagName("TABLE")(0).Style =
+                "FONT-SIZE: 95%; BORDER-TOP: #282828 1px solid; BORDER-RIGHT: #282828 1px solid; WIDTH: 100%; BACKGROUND: #282828; BORDER-BOTTOM: #282828 1px solid; TEXT-ALIGN: center; BORDER-LEFT: #282828 1px solid"
+
+        Catch
+        End Try
+
+        If senses = 1 Then
+            display.Text = "1 sense | " + qterm
+            displayMem = "1 sense | " + qterm
+        Else
+            display.Text = senses.ToString() + " senses | " + qterm
+            displayMem = senses.ToString() + " senses | " + qterm
+        End If
+
+        'The browser's been hidden this whole time to save your eyes
+        auxBrowser.Show()
+        ghost.Select()
+
+    End Sub
+
+    Private Sub MainFrame_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Load up the list of colors we want to switch to a different color
+        map.Add("#c0cfe4", "#5479AD")
+        map.Add("#ccc", "#656565")
+        map.Add("#c0e4c0", "#489148")
+        map.Add("#e4d4c0", "#9E7B53")
+        map.Add("#e0e0b0", "#9E9E3C")
+        map.Add("#e2e4c0", "#B5B529")
+        map.Add("#f8f8ff", "#404040")
+        map.Add("#40e0d0", "#38C4B6")
+    End Sub
+
+    Private Sub interLoad_Tick(sender As Object, e As EventArgs) Handles interLoad.Tick
+        'Hide on start (for some reason Form.Load is unreliable)
+        interLoad.Enabled = False
+        Hide()
+    End Sub
+
+    Private Sub auxBrowser_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles auxBrowser.Navigating
+
+        'UI stuff
+        qterm = e.Url.OriginalString.Replace("https://en.wiktionary.org/wiki/", "").Replace("#Latin", "")
+        display.Text = "Querying " & qterm & "..."
+        displayMem = "Querying " & qterm & "..."
+        ghost.Select()
+
+        'Maintain a history for the back/forward buttons
+        If (prev_ind = prev.Count - 1) And (prev_ind = -1 OrElse Not prev(prev_ind) = qterm) Then
+            prev.Add(qterm)
+            prev_ind += 1
+        End If
+
+        'UI stuff
+        Height = 25
+        Width = 400
+        If firstopen Then
+            x = MousePosition.X
+            y = MousePosition.Y
+            firstopen = False
+        End If
+        rsz()
+
+        'If clicking a link on the wiki, sometimes it gets clipped, so this just makes sure it's a proper link
+        If e.Url.OriginalString.StartsWith("about:") Then
+
+            e.Cancel = True
+            prev.RemoveAt(prev_ind)
+            prev_ind -= 1
+            auxBrowser.Navigate(e.Url.OriginalString.Replace("about:", "https://en.wiktionary.org"))
+
+        End If
+
+    End Sub
+
+    'Normal VB UI stuff
+    Sub rsz()
+        auxBrowser.Top = 25
+        auxBrowser.Left = 0
+        auxBrowser.Height = Height - 25
+        auxBrowser.Width = Width + 20
+        display.Width = Width - 60
+        If x + Width > Screen.PrimaryScreen.WorkingArea.Width Then
+            Left = x - Width
+
+        Else
+            Left = x
+        End If
+        If y + Height > Screen.PrimaryScreen.WorkingArea.Height Then
+            Top = y - Height
+        Else
+            Top = y
+        End If
+    End Sub
+
+    Dim displayMem As String = ""
+
+    Private Sub display_Enter(sender As Object, e As EventArgs) Handles display.Enter
+        displayMem = display.Text
+        display.Text = ""
+    End Sub
+
+    Private Sub display_Leave(sender As Object, e As EventArgs) Handles display.Leave
+        display.Text = displayMem
+    End Sub
+
+    Private Sub display_KeyDown(sender As Object, e As KeyEventArgs) Handles display.KeyDown
+        If e.KeyCode = Keys.Return Then
+            prev.Clear()
+            prev_ind = -1
+            auxBrowser.Hide()
+            auxBrowser.Navigate("https://en.wiktionary.org/wiki/" & display.Text.Replace("ā", "a").Replace("ē", "e").Replace("ī", "i").Replace("ō", "o").Replace("ū", "u"))
+        End If
+        If e.KeyCode = Keys.Escape Then
+            ghost.Select()
+        End If
+    End Sub
+
+    Private Sub b_forward_Click(sender As Object, e As EventArgs) Handles b_forward.Click
+        If prev_ind >= prev.Count - 1 Then
+            Return
+        End If
+        prev_ind += 1
+        auxBrowser.Navigate("https://en.wiktionary.org/wiki/" & prev(prev_ind))
+    End Sub
+
+    Private Sub buttonCheck_Tick(sender As Object, e As EventArgs) Handles buttonCheck.Tick
+        If prev_ind > 0 Then
+            b_back.Enabled = True
+        Else
+            b_back.Enabled = False
+        End If
+        If prev_ind < prev.Count - 1 Then
+            b_forward.Enabled = True
+        Else
+            b_forward.Enabled = False
+        End If
+    End Sub
+
+    Private Sub b_back_Click(sender As Object, e As EventArgs) Handles b_back.Click
+        If prev_ind <= 0 Then
+            Return
+        End If
+        prev_ind -= 1
+        auxBrowser.Navigate("https://en.wiktionary.org/wiki/" & prev(prev_ind))
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        nicon.Dispose()
+        Application.Exit()
+    End Sub
+
+    Private Sub moveTimer_Tick(sender As Object, e As EventArgs) Handles moveTimer.Tick
+
+        If MousePosition.X + Width < Screen.PrimaryScreen.WorkingArea.Width Then
+            Left = MousePosition.X
+        Else
+            Left = Screen.PrimaryScreen.WorkingArea.Width - Width
+        End If
+        If MousePosition.Y + Height < Screen.PrimaryScreen.WorkingArea.Height Then
+            Top = MousePosition.Y
+        Else
+            Top = Screen.PrimaryScreen.WorkingArea.Height - Height
+        End If
+
+        x = Left
+        y = Top
+
+    End Sub
+End Class
+
+'Also not my code, this is a very useful class that I pulled off the link below which gets keypresses without focus
+'https://sim0n.wordpress.com/2009/03/28/vbnet-keyboard-hook-class/
+Public Class KeyboardHook
+
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)>
+    Private Overloads Shared Function SetWindowsHookEx(ByVal idHook As Integer, ByVal HookProc As KBDLLHookProc, ByVal hInstance As IntPtr, ByVal wParam As Integer) As Integer
+    End Function
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)>
+    Private Overloads Shared Function CallNextHookEx(ByVal idHook As Integer, ByVal nCode As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+    End Function
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)>
+    Private Overloads Shared Function UnhookWindowsHookEx(ByVal idHook As Integer) As Boolean
+    End Function
+
+    <StructLayout(LayoutKind.Sequential)>
+    Private Structure KBDLLHOOKSTRUCT
+        Public vkCode As UInt32
+        Public scanCode As UInt32
+        Public flags As KBDLLHOOKSTRUCTFlags
+        Public time As UInt32
+        Public dwExtraInfo As UIntPtr
+    End Structure
+
+    <Flags()>
+    Private Enum KBDLLHOOKSTRUCTFlags As UInt32
+        LLKHF_EXTENDED = &H1
+        LLKHF_INJECTED = &H10
+        LLKHF_ALTDOWN = &H20
+        LLKHF_UP = &H80
+    End Enum
+
+    Public Shared Event KeyDown(ByVal Key As Keys)
+    Public Shared Event KeyUp(ByVal Key As Keys)
+
+    Private Const WH_KEYBOARD_LL As Integer = 13
+    Private Const HC_ACTION As Integer = 0
+    Private Const WM_KEYDOWN = &H100
+    Private Const WM_KEYUP = &H101
+    Private Const WM_SYSKEYDOWN = &H104
+    Private Const WM_SYSKEYUP = &H105
+
+    Private Delegate Function KBDLLHookProc(ByVal nCode As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+
+    Private KBDLLHookProcDelegate As KBDLLHookProc = New KBDLLHookProc(AddressOf KeyboardProc)
+    Private HHookID As IntPtr = IntPtr.Zero
+
+    Private Function KeyboardProc(ByVal nCode As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+        If (nCode = HC_ACTION) Then
+            Dim struct As KBDLLHOOKSTRUCT
+            Select Case wParam
+                Case WM_KEYDOWN, WM_SYSKEYDOWN
+                    RaiseEvent KeyDown(CType(CType(Marshal.PtrToStructure(lParam, struct.GetType()), KBDLLHOOKSTRUCT).vkCode, Keys))
+                Case WM_KEYUP, WM_SYSKEYUP
+                    RaiseEvent KeyUp(CType(CType(Marshal.PtrToStructure(lParam, struct.GetType()), KBDLLHOOKSTRUCT).vkCode, Keys))
+            End Select
+        End If
+        Return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam)
+    End Function
+
+    Public Sub New()
+        HHookID = SetWindowsHookEx(WH_KEYBOARD_LL, KBDLLHookProcDelegate, System.Runtime.InteropServices.Marshal.GetHINSTANCE(System.Reflection.Assembly.GetExecutingAssembly.GetModules()(0)).ToInt32, 0)
+        If HHookID = IntPtr.Zero Then
+            Throw New Exception("Could not set keyboard hook")
+        End If
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        If Not HHookID = IntPtr.Zero Then
+            UnhookWindowsHookEx(HHookID)
+        End If
+        MyBase.Finalize()
+    End Sub
+
+End Class
